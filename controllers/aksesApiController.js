d3ba01ffeb2d69ad8e8047628d48c66dd7d4725a
Monohiks <minejas179@gmail.com>
2025-06-13 20:19:19 +0700

    add new parameter to aksesApi

--- controllers/aksesApiController.js
+++ controllers/aksesApiController.js
@@ -9,7 +9,11 @@ class AksesApiController {
             const limit = parseInt(req.query.limit) || 10;
             const method = req.query.method;
             const status_code = req.query.status_code ? parseInt(req.query.status_code) : null;
+            
+            const start_date = req.query.start_date || null; // Format: YYYY-MM-DD
+            const end_date = req.query.end_date || null;     // Format: YYYY-MM-DD
 
+            // Mengembalikan data log API dalam bentuk JSON response ke client.
             const result = await AksesApiModel.getAll(page, limit, method, status_code);
             res.status(200).json(successResponse('Riwayat akses API berhasil diambil', result.data, result.pagination));
 
